33b3ac0fbf151dcd2fc32066538c790643e7dc1c
Sumsps <sums.ps@aol.com>
2016-10-31 00:28:05 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,83 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# makeVector <- function(x = numeric()) {
+#     m <- NULL
+#     set <- function(y) {
+#         x <<- y
+#         m <<- NULL
+#     }
+#     get <- function() x
+#     setmean <- function(mean) m <<- mean
+#     getmean <- function() m
+#     list(set = set, get = get,
+#          setmean = setmean,
+#          getmean = getmean)
+# }
+# 
+# cachemean <- function(x, ...) {
+#     m <- x$getmean()
+#     if(!is.null(m)) {
+#         message("getting cached data")
+#         return(m)
+#     }
+#     data <- x$get()
+#     m <- mean(data, ...)
+#     x$setmean(m)
+#     m
+# }
 
-makeCacheMatrix <- function(x = matrix()) {
+# Above sample code is used to create the logic.
 
+#####################################################################################################    
+# Created by - Sumesh Sukumaran 
+# Created date - 10/30/2016
+# Status  - Completed
+# Comments - Assignment Week 3 - Matrix inversion & storing values in memory
+#####################################################################################################
+
+#Function to create a matrix object & manag cache
+makeCacheMatrix <- function(obj = matrix()) 
+{
+    matrixcache <- NULL
+    setMatrix <- function(newMatrix) 
+    {
+        obj <<- newMatrix
+        matrixcache <<- NULL
+    }
+    
+    getMatrix <- function() { obj }
+
+    setCache <- function(InverseMatrix) 
+    { 
+        #Cache is assigned to variable.
+        matrixcache <<- InverseMatrix 
+        
+    }
+    
+    getCache <- function() 
+    { 
+        matrixcache 
+    }
+    
+    list(setMatrix = setMatrix
+         , getMatrix = getMatrix
+         , setCache = setCache
+         , getCache = getCache)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#Function to access inverse matrix data from cache or calculate if not exist in memory. 
+cacheSolve <- function(MatrixObj, ...) 
+{
+    inverseMatrix <- MatrixObj$getCache()
+    if(is.null(inverseMatrix)==FALSE) 
+    {
+        print("Retrieving from Cache")
+        return(inverseMatrix)
+    }
+    
+    print("No cache found")
+    inputMatrix <- MatrixObj$getMatrix()
+    inverseMatrix <- solve(inputMatrix)
+    MatrixObj$setCache(inverseMatrix)
+    print("New cache created")
+    return(inverseMatrix)
 }
